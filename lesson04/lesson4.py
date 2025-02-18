3d74077ce93a46a99aab5876534ca15f41e0befe
oxmedium <dankdragon841@gmail.com>
2025-02-18 10:45:33 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,13 @@
 # Lesson 4
+import math
+
+fence1 = input()
+fence2 = input()
+fence3 = input()
+length = len(fence1) + len(fence2) + len(fence3)
+extra = length % 12
+paint = math.ceil(length / 12)
+cost = paint * 14.95
+print(length)
+print(extra)
+print(cost)
