7e13bb8feba87493210fabb5e95fc7f9f195c176
oxmedium <dankdragon841@gmail.com>
2025-02-19 14:39:43 -0500

    solved lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,10 @@
 # Lesson 7
+import random 
+
+dc = int(input("Enter the difficulty level: "))
+num = random.randrange(1, 21)
+print(f"The player has rolled a {num}!")
+if dc > num:
+    print("Fail..")
+elif dc <= num:
+    print("Success!")
