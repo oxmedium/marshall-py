530cfc2a622d8c3d6a132f97d952160db8d3f865
oxmedium <dankdragon841@gmail.com>
2025-02-19 14:57:24 -0500

    solved lesson 8

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,16 @@
 # Lesson 8
+winloss = input("Input your wins and losses: ")
+wins = 0
+for i in range(6):
+    game = input(f"What was the results of game {i + 1} ")
+    if game == "W":
+        wins += 1
+
+if wins > 4:
+    print("You are in group 1")
+elif wins > 2:
+    print("You are in group 2")
+elif wins > 0:
+    print("You are in group 3")
+else:
+    print("You are a loser dawg")
