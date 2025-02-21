6406040c34e671e9cf2a0ca086b95651c47f1499
oxmedium <dankdragon841@gmail.com>
2025-02-21 14:03:11 -0500

    solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,13 @@
 # Lesson 10
+num = input("What is the number? ")
+
+if num[0] in {'8', '9'}:
+    if num[1] == num[2]:
+        if num[-1] in {'8', '9'}:
+            print("Telemarketer")
+        else:
+            print("Not Telemarketer")
+    else:
+        print("Not Telemarketer")
+else:
+    print("Not Telemarketer")
