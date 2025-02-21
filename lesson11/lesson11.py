09b2bcc18ace314a91f3364ffc8651922bebdd6e
oxmedium <dankdragon841@gmail.com>
2025-02-21 14:40:23 -0500

    solved lesson 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,22 @@
 # Lesson 11
+point = input("Input a point: ")
+
+point = point.split(' ')#turns 1 1 -> ('1', '1')
+
+point = list(map(int, point))
+print(point)
+
+x, y = point
+print(f"The x coordinate is: {x}")
+print(f"The y coordinate is: {y}")
+
+if x > 0:
+    if y > 0:
+        print("Quadrant 1")
+    else:
+        print("Quadrant 4")
+else:
+    if y > 0:
+        print("Quadrant 2")
+    else:
+        print("Quadrant 3")
