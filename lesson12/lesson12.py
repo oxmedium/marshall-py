5041474739621c42174716258570afd3f80516ea
oxmedium <dankdragon841@gmail.com>
2025-02-23 16:14:43 -0500

    solved problem 12

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,16 @@
 # Lesson 12
+angles = 0
+while angles != 180:
+    a = int(input("Enter the 3 angles of the triangle: "))
+    b = int(input())
+    c = int(input())
+    angles = a + b + c
+    if angles != 180:
+        print("Angles need to add up to 180!")
+
+if a == b == c:
+    print("Equilateral")
+elif a == b or b == c or a == c:
+    print("Isoceles")
+else:
+    print("Scalene")
