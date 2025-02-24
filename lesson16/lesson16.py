50a527298c51e038fde0c5b8973102423b34ddb8
oxmedium <dankdragon841@gmail.com>
2025-02-24 17:57:50 -0500

    solved lesson 16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,10 @@
 # Lesson 16
+for i in range(1,51):
+    if i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
