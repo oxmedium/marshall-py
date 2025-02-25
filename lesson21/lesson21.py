b1c495854c67070c936d0b522e6126ceb285775d
oxmedium <dankdragon841@gmail.com>
2025-02-24 21:51:08 -0500

    solved lesson 21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,13 @@
 # Lesson 21
+num = int(input("Enter the highest number to go up to: "))
+high_num = 0
+most = 0
+for i in range(1, num):
+    counter = 0
+    for j in range(1, i):
+        if i % j == 0:
+            counter += 1
+    if counter > most:
+        most = counter
+        high_num = i
+print(f"The number with the highest amount of factors between 1 and {num} is {high_num} with {most} factors.")
