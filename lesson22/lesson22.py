2e8f080a78076a3b18f846307a17f508165ba7bc
oxmedium <dankdragon841@gmail.com>
2025-02-24 22:44:27 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,14 @@
 # Lesson 22
+n = int(input("What Nth sequence of the Fibonacci sequence do you want: "))
+one = 0
+two = 1
+three = 0
+
+for i in range (2, n+1):
+    three = one + two
+    one = two
+    two = three
+if n == 1:
+    print(two)
+else:
+    print(three)
