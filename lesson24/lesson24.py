f94b05aa0a2876f9c22ff87cbf2ba38407210255
oxmedium <dankdragon841@gmail.com>
2025-02-25 09:36:14 -0500

    solved lesson 24

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,9 @@
 # Lesson 24
+temp = ""
+while True:
+    user = input("Input names, X to stop: ")
+    if user.capitalize() == "X":
+        break
+    elif len(user) > len(temp):
+        temp = user
+print(f"The longest name inputted was {temp}")
