7f9516730848c5486ae3c1ce21523989260d64f9
oxmedium <dankdragon841@gmail.com>
2025-02-25 14:58:14 -0500

    solved lesson 25

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,17 @@
 # Lesson 25
+num = int(input("Enter a number to find its highest prime factor: "))
+numcopy = num
+
+largest = 0
+while num % 2 == 0:
+    num //= 2
+    largest = max(largest, 2)
+if num != 1:
+    factor = 3
+    while num != 1:
+        if num % factor == 0:
+            num //= factor
+            largest = max(largest, factor)
+        else:
+            factor += 2
+print(f"The highest prime factor of {numcopy} is {largest}")
