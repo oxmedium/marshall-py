fcec86946deb7f81592d166af498d9ea438cd625
oxmedium <dankdragon841@gmail.com>
2025-02-24 20:33:05 -0500

    solved lesson 19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,11 @@
 # Lesson 19
+num = int(input("Prime number checker: "))
+prime = True
+for i in range(2, num):
+    if num % i == 0:
+        prime = False
+        break
+if prime:
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
