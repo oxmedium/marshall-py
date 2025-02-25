4ffa5277b798b9fbeb7fc7f662eb2a3c3e12b7cb
oxmedium <dankdragon841@gmail.com>
2025-02-24 21:35:05 -0500

    solved lesson 20

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,13 @@
 # Lesson 20
+print("All Perfect number under 10000: ")
+total = 0
+for num in range(1,10000):
+    sum = 0
+    for i in range(1,num):
+        if num % i == 0:
+            sum += i
+    if sum == num:
+        print(num)
+        total += sum
+print(f"{total} is the total sum of all the perfect numbers under 10000")
+
