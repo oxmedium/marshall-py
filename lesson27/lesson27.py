e00085be07c1218bf79e963c69de5dcba858cc64
oxmedium <dankdragon841@gmail.com>
2025-03-12 18:35:00 -0400

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,10 @@
 # Lesson 27
+def cleaner(text):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            result += char
+    return result
+
+new = cleaner('mike_oxlong')
+print(new)
