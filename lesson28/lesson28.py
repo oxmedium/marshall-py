46287bc36c995c482e3fb40b1442a1344d9998ac
oxmedium <dankdragon841@gmail.com>
2025-03-17 14:48:00 -0400

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,21 @@
 # Lesson 28
+''' Solution 1
+def palindrome(word):
+    return word == word[::-1]
+
+print(palindrome('tacocat'))
+'''#Solution 2
+def palindrome(word):
+    if not word:
+        return True
+    elif len(word) < 4:
+        return word[0] == word[-1]
+    else:
+        mid = len(word) // 2
+        for i in range(0, mid):
+            l = i
+            r = -1*i -1
+            if word[l] != word[r]:
+                return False
+        return True
+print(palindrome('tcat'))
