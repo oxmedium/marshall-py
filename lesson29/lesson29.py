93d50dff34d9f35c839b70d922b0b717559c2cc6
oxmedium <dankdragon841@gmail.com>
2025-03-20 14:21:26 -0400

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,16 @@
 # Lesson 29
+def consonent(text, vowel=False):
+    total = 0
+    for character in text:   
+        if character.lower().isalpha and character not in {"a", "e", "i", "o", "u"}:
+            total += 1
+    if not vowel:
+        return total
+    else:
+        total = 0
+        for character in text:
+            if character.lower().isalpha and character in {"a", "e", "i", "o", "u"}:
+                total += 1
+        return total
+print(f"Count of {consonent("hello")}")
+print(f"Count of {consonent("hello,", True)}")
