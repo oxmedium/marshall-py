a75b41b8e923058b18ef07555117d81ba9ce974e
oxmedium <dankdragon841@gmail.com>
2025-03-20 14:51:20 -0400

    solved lesson 30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,14 @@
 # Lesson 30
+def line(num):
+    text = ''
+    for i in range(1,num+1):
+        if i % 2 == 0:
+            text += '0'
+        else:
+            text += '1'
+    return text
+def output(num):
+    for i in range (1,num+1):
+        print(line(i))
+ber = output(5)
+print(ber)
