55d89b2c39f7bb046bf08c9d90b1d51ac07032d6
oxmedium <dankdragon841@gmail.com>
2025-03-21 14:32:57 -0400

    solved lesson 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,23 @@
 # Lesson 31
+#method 1
+def anagram(word1, word2):
+    list1 = sorted(word1)
+    list2 = sorted(word2)
+    if list1 == list2:
+        return True
+    else:
+        return False
+print(anagram("mike", "ekim"))
+
+#method 2
+
+def anagram1(word3, word4):
+    if len(word3) != len(word4):
+        return False
+    else:
+        for char in word3:
+            if word3.count(char) != word4.count(char):
+                return False
+    return True
+print(anagram1("mike", "ekim"))
+print(anagram1("michael", 'mikey'))
