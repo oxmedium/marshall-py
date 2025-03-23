75967faa507f063276d4c5e3ff67f1dd2586b59e
oxmedium <dankdragon841@gmail.com>
2025-03-22 23:29:41 -0400

    solved lesson 35

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,10 @@
 # Lesson 35
+def remove_dupes(a_list):
+    new_list = []
+    for item in a_list:
+        if item not in new_list:
+            new_list.append(item)
+    return new_list
+test = ['a', 'b', 'c', 'c', 'b', 'c', 'a', 'a', 'd']
+print(test)
+print(remove_dupes(test))
