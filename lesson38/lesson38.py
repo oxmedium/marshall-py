c7c367d15a3796c56f798583b9d21943e9d2abd6
oxmedium <dankdragon841@gmail.com>
2025-03-23 19:27:04 -0400

    solved lesson 38

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,13 @@
 # Lesson 38
+def palindrome(text):
+    return text[::-1] == text
+
+palindromes = []
+
+for num1 in range(999,99,-1):
+    for num2 in range(999,99,-1):
+        num = num1 * num2
+        if palindrome(str(num)):
+            palindromes.append(num)
+
+print(max(palindromes))
