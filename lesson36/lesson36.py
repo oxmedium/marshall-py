79bb77070b514a7b356e901954c9a98cbd1a5416
oxmedium <dankdragon841@gmail.com>
2025-03-22 23:43:21 -0400

    solved lesson 36

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,17 @@
 # Lesson 36
+def factor(num):
+    factors = []
+    for i in range(1,num+1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
+def primechecker(num):
+    prime = False
+    if len(factor(num)) == 2:
+        prime = True
+    return prime
+
+print(f"factors of 13: {factor(13)}")
+print(f"factors of 26: {factor(26)}")
+print(f"is 13 prime: {primechecker(13)}")
+print(f"is 26 prime: {primechecker(26)}")
