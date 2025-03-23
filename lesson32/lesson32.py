a01cced164d5e6c0832a581c1e56674d14535fb5
oxmedium <dankdragon841@gmail.com>
2025-03-22 21:48:59 -0400

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,14 @@
 # Lesson 32
+
+def add(word1,word2):
+    set1 = set(word1)
+    set2 = set(word2)
+    letters = []
+    if not word1 and not word2:
+        return []
+    else:
+        for character in set1:
+            if character in set2:
+                letters.append(character)
+        return sorted(letters)
+print(add("hello world", "goodbye world"))
