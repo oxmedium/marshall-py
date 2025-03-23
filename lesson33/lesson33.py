1fadeacb9f00fc3783e63c2823a4f7462a5ad298
oxmedium <dankdragon841@gmail.com>
2025-03-22 22:18:20 -0400

    solved lesson 33

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,22 @@
 # Lesson 33
+def mean(nums):
+    avg = 0
+    if str(sum(nums)).isnumeric():
+        avg = sum(nums) / len(nums)
+        return avg
+    else:
+        return avg
+def median(nums):
+    bers = sorted(nums)
+    mid = 0
+    if len(bers) % 2 == 0:
+        right = bers[len(nums) // 2]
+        left = bers[len(nums) // 2 - 1]
+        mid = (right + left) / 2
+        return mid
+    else:
+        mid = bers[len(nums) // 2]
+        return mid
+num = [2,2,3,10,6,7]
+print(f"Mean: {mean(num)}")
+print(f"Median: {median(num)}")
