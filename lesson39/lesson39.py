2c301bb50c2acc042f4e3ec51d5a73435c9a3ded
oxmedium <dankdragon841@gmail.com>
2025-03-23 20:44:46 -0400

    solved lesson39

--- lesson39/lesson39.py
+++ lesson39/lesson39.py
@@ -1,1 +1,19 @@
 # Lesson 39
+def factor(num1,num2):
+    for i in range(min(num1,num2),0,-1):
+        if num1 % i == 0 and num2 % i == 0:
+            return i
+    return 1
+
+print(factor(4,12))
+print(factor(27,1080))
+
+def e_gcd(num1,num2):
+    if num2 == 0:
+        return num1
+    else:
+        return e_gcd(num2, num1 % num2)
+    
+print(e_gcd(8,12))
+print(e_gcd(27,1080))
+    
