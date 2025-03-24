07485f8cec1bdf1f8c57b0e3b4501ba19f20a8d3
oxmedium <dankdragon841@gmail.com>
2025-03-24 14:23:34 -0400

    solved lesson 45

--- lesson45/lesson45.py
+++ lesson45/lesson45.py
@@ -1,1 +1,18 @@
 # Lesson 45
+def dictionary(a_list):
+    return {key: len(key) for key in a_list}
+
+test = ('apple', 'banana', 'cherry', 'date')
+print(dictionary(test))
+
+#Fibonnaci
+def fib(num):
+    sequence = {0:0, 1:1}
+    if num in sequence:
+        return sequence
+    else:
+        for i in range(2,num+1):
+            sequence[i] = sequence[i-1] + sequence[i-2]
+        return sequence
+
+print(fib(10))
