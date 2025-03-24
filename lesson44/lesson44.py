b64ff4e4fe1bc1935a0ff41bd787de388666a4f8
oxmedium <dankdragon841@gmail.com>
2025-03-24 11:56:20 -0400

    solved lesson 44

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,12 @@
 # Lesson 44
+def dictionary(text):
+    word = sorted(text.lower())
+    answer = {}
+    for letter in word:
+        if letter not in answer:
+            answer[letter] = 1
+        else:
+            answer[letter] += 1
+    return answer
+test = "hello"
+print(dictionary(test))
